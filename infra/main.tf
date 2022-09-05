382076e75abd66974e80419c53365225f162f015
Eduardo Borges Brasil <eduardobrl10@gmail.com>
2022-09-05 13:12:59 -0300

    change instace class

--- infra/main.tf
+++ infra/main.tf
@@ -33,7 +33,7 @@ provider "aws" {
 # RDS
 resource "aws_db_instance" "pedidoaberto_writedb" {
   identifier             = "pedidoaberto-writedb"
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
   engine_version         = "14.4"
